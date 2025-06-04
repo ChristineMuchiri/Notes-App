418266e2b0b1ed36a21fd918b13a6490697c2607
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-04 20:54:14 +0300

    api

--- notes-api/get_note.py
+++ notes-api/get_note.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodbb')
+dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('NotesTable')
 
 def lambda_handler(event, context):
