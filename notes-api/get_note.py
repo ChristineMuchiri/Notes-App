a060a25b27e747832120bff75a258951c5d81942
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-04 20:50:30 +0300

    notes-app api

---
+++ notes-api/get_note.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodbb')
+table = dynamodb.Table('NotesTable')
+
+def lambda_handler(event, context):
+    note_id = event['pathParameters']['id']
+    response = table.get_item(Key={'noteId': note_id})
+    
+    item = response.get('Item')
+    if not item:
+        return {'statusCode': 404, 'body': json.dumps({'message': 'Note not found'})}
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(item)
+    }
